f10254e4048e78376b13f45aefb70eb96800dc1f
Vinh Hoang <vinhhn3@fe.edu.vn>
2023-11-20 13:35:22 +0700

    introduce to class component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Properties
+  state = {
+    fullName: "Vinh Hoang", // read only
+    age: 18,
+  };
+
+  // Methods
+  increaseAge = () => {
+    this.setState({ age: this.state.age + 1 });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World</h1>
+        <p>My name is {this.state.fullName}</p>
+        <p>I am {this.state.age} years old</p>
+        <button onClick={this.increaseAge}>Increase age</button>
+      </div>
+    );
+  }
 }
 
 export default App;
