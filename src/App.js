a3b94e744d3176f9dbf4c4fbdfa9d10bfa572159
Vinh Hoang <vinhhn3@fe.edu.vn>
2023-11-20 14:47:25 +0700

    refactor

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
+import axios from "axios";
 import { Component } from "react";
 import "./App.css";
 import Users from "./components/Users";
 import Navbar from "./layout/Navbar";
 
 class App extends Component {
+  state = {
+    usersData: [],
+  };
+  componentDidMount() {
+    axios.get("https://api.github.com/users").then((response) => {
+      this.setState({ usersData: response.data });
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users />
+          <Users usersData={this.state.usersData} />
         </div>
       </div>
     );
