b2d73a91bd44e321d9183b02a24f4dfffb296849
Vinh Hoang <vinhhn3@fe.edu.vn>
2023-11-20 15:07:24 +0700

    Add Search

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import axios from "axios";
 import { Component } from "react";
 import "./App.css";
+import Search from "./components/Search";
 import Users from "./components/Users";
 import Navbar from "./layout/Navbar";
 
 class App extends Component {
   state = {
     usersData: [],
+    searchText: "",
   };
-  componentDidMount() {
-    axios.get("https://api.github.com/users").then((response) => {
-      this.setState({ usersData: response.data });
-    });
-  }
+
+  handleSearch = (text) => {
+    console.log(text);
+    axios
+      .get(`https://api.github.com/search/users?q=${text}`)
+      .then((response) => {
+        console.log(response);
+        this.setState({ usersData: response.data.items });
+      });
+  };
 
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
+          <Search handleSearch={this.handleSearch} />
           <Users usersData={this.state.usersData} />
         </div>
       </div>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,34 @@
 import React from "react";
 
 export default class Search extends React.Component {
+  state = {
+    text: "",
+  };
+
+  handleClick = (e) => {
+    e.preventDefault(); // Stop form reloading
+    console.log(this.state.text);
+    this.props.handleSearch(this.state.text);
+  };
+
+  handleChange = (e) => {
+    console.log(e.target.value);
+    this.setState({ text: e.target.value });
+  };
+
   render() {
     return (
       <div>
-        <p>Search component</p>
+        <form>
+          <input
+            type="text"
+            placeholder="Search"
+            onChange={this.handleChange}
+          />
+          <button onClick={this.handleClick} className="btn btn-dark btn-block">
+            Search
+          </button>
+        </form>
       </div>
     );
   }
